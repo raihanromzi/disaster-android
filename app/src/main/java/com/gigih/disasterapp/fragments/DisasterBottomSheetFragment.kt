fadf5efae9483a75f2b96db98ee7cf55635ecc87
raihanromzi <raihan.rakhman16@gmail.com>
2023-07-22 22:34:48 +0700

    fix bottom sheet

--- app/src/main/java/com/gigih/disasterapp/fragments/DisasterBottomSheetFragment.kt
+++
@@ -1,5 +0,0 @@
-package com.gigih.disasterapp.fragments
-
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
-class DisasterBottomSheetFragment : BottomSheetDialogFragment()
