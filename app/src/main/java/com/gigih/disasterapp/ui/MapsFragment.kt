649235ae6588424be6d94f1e98db7c2148233aa3
raihanromzi <raihan.rakhman16@gmail.com>
2023-08-13 18:49:09 +0700

    add disaster location on map

--- app/src/main/java/com/gigih/disasterapp/ui/MapsFragment.kt
+++ app/src/main/java/com/gigih/disasterapp/ui/MapsFragment.kt
@@ -5,17 +5,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.gigih.disasterapp.R
-import com.google.android.gms.maps.CameraUpdateFactory
+import com.gigih.disasterapp.data.remote.response.GeometriesItem
+import com.gigih.disasterapp.ui.viewmodel.MainViewModel
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment(), OnMapReadyCallback {
+class MapsFragment : Fragment() {
 
-    private var mMap: GoogleMap? = null
+    private val mainViewModel by viewModels<MainViewModel>()
+    private val boundsBuilder = LatLngBounds.Builder()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,20 +32,32 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
+        mapFragment?.getMapAsync(callback)
     }
 
-    override fun onMapReady(p0: GoogleMap) {
-        val sydney = LatLng(
-            -6.12562,
-            106.784538
-        )
-        p0.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        p0.addMarker(
-            MarkerOptions().position(LatLng(-7.12562, 105.784538)).title("Marker in Sydney 2")
-        )
 
-        p0.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun addDisasterMarker(map: GoogleMap, disasters: List<GeometriesItem>) {
+        map.clear()
+
+        disasters.forEach { disaster ->
+            val lat = disaster.coordinates[1].toString().toDouble()
+            val lon = disaster.coordinates[0].toString().toDouble()
+
+            val latLng = LatLng(lat, lon)
+            map.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title(disaster.properties.title)
+                    .snippet(disaster.properties.createdAt)
+            )
+            boundsBuilder.include(latLng)
+        }
+    }
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        mainViewModel.disasters.observe(this) { disasters ->
+            addDisasterMarker(googleMap, disasters)
+        }
     }
 
 
